43537320cd383886a8cad9b8e03381a98f52070e
Adimno <jayjaynaval02@gmail.com>
2024-11-22 00:21:21 +0800

    remove firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:thirst_tea/sign_up/sign_up_page.dart';
-import 'firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:thirst_tea/sign_in/sign_in_page.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter engine is initialized before Firebase
 
-
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    ); // Initialize Firebase
-
-  } catch (e) {
-    // Handle initialization error
-    print("Firebase initialization error: $e");
-  }
   runApp(const MyApp());
 }
 
@@ -28,7 +16,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
